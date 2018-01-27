85d9bdf33c89d4094766fa9599e787c02be9c11f
Ryan Sidebottom <rmsidebottom0@gmail.com>
2018-01-26 22:14:39 -0500

    Adding more cases and flags. Working on figuring out the proper cases and what the flags mean

--- HostDiscovery/discovery.py
+++ HostDiscovery/discovery.py
@@ -6,19 +6,39 @@ from scapy.all import *
 # return: true of false pending on response from remote host
 def icmpEcho(ip):
     packet = IP(dst=ip)/ICMP(type=8)
-    ans = sr1(packet)
-    return
+    ans = sr1(packet, timeout=2, verbose=0)
+
+    if ans is None:
+        return False
+    elif (ans.getlayer(ICMP).type==3 and
+        ans.getlayer(ICMP).code in [1,2,3,9,10,13]):
+        return False
+    else:
+        return True
 
 # send TCP SYN to port 443 on ip
 # ip: the IP address of a potential host
 # return: true of false pending on response from remote host
 def tcpSYN(ip):
+    # system that is up returns RST, ACK
+    # linux sends ICMP destination unreachable when firewall on
+    packet = IP(dst=ip)/TCP(sport=5000, dport=80, flags="S")
+    ans = sr1(packet, timeout=2, verbose=0)
+
+    if ans is None:
+        return False
+    elif (ans.getlayer(TCP).flags=="20"):
+        return True
+    else:
+        return False
     return
 
 # send TCP ACK to port 80 on ip
 # ip: the IP address of a potential host
 # return: true of false pending on response from remote host
 def tcpACK(ip):
+    # receives an RST back
+    # linux sends ICMP destination unreachable when firewall on
     return
 
 # send ICMP Timestamp request to ip
@@ -28,6 +48,16 @@ def tcpACK(ip):
 def icmpTimestamp(ip):
     packet = IP(dst=ip)/ICMP(type=13)
     ans = sr1(packet)
+
+    if ans is None:
+        return False
+    elif (ans.getlayer(ICMP).type==3 and
+        ans.getlayer(ICMP).code in [1,2,3,9,10,13]):
+        return False
+    else:
+        return True
+
+
     return
 
 def main():
